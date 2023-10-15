a79544956c836299b5f462a1b5d86a768c1df624
Theo Paesch <tpaesch@2-H-7.42heilbronn.de>
2023-10-15 14:57:49 +0200

    push it

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,8 +6,31 @@
 /*   By: tpaesch <tpaesch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 15:02:43 by tpaesch           #+#    #+#             */
-/*   Updated: 2023/10/09 13:20:43 by tpaesch          ###   ########.fr       */
+/*   Updated: 2023/10/15 13:02:14 by tpaesch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (needle[i] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j]
+			&& haystack[i + j] != '\0' && (i + j) < len)
+		{
+			j++;
+			if (needle[j] == '\0')
+				return ((char *) &haystack[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
